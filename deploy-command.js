cfee16c678863e304b332df8b7eebec076fc3272
Ricky Mao <rickyrmao@gmail.com>
2021-08-31 18:54:56 -0700

    Added bot setup

---
+++ deploy-command.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config/config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+    new SlashCommandBuilder().setName('laugh').setDescription('Joins your call to play a laugh track')
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully registered application commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
---
+++ index.js
@@ -0,0 +1,72 @@
+const { Client, Intents } = require('discord.js');
+const {
+	AudioPlayerStatus,
+	StreamType,
+	createAudioPlayer,
+	createAudioResource,
+	joinVoiceChannel,
+	entersState,
+	NoSubscriberBehavior,
+	VoiceConnectionStatus,
+} = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const { join } = require('path');
+const { createReadStream } = require('fs');
+
+const player = createAudioPlayer({
+	behaviors: {
+		noSubscriber: NoSubscriberBehavior.Play,
+	},
+});
+
+player.on('error', error => {
+	console.error('Error:', error.message, 'with track', error.resource.metadata.title);
+});
+
+const client = new Client({ intents: ['GUILD_VOICE_STATES', 'GUILD_MESSAGES', 'GUILDS'] });
+client.login('ODc2NzIyNTEwNzc3OTQyMDI2.YRoNdw.M35K3empYohkC16hpKo6ufGD3ps');
+
+
+client.once('ready', () => {
+	try {
+		console.log('Ready!');
+	} catch (e) {
+		console.log({ e });
+	}
+
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) { return; }
+
+	if (interaction.commandName === "laugh") {
+		const channel = interaction.member.voice.channel;
+		const connection = joinVoiceChannel({
+			channelId: channel.id,
+			guildId: channel.guild.id,
+			adapterCreator: channel.guild.voiceAdapterCreator
+		});
+
+		try {
+			await entersState(connection, VoiceConnectionStatus.Ready, 20e3);
+		} catch (e) {
+			console.log({ e });
+		}
+
+		connection.on(VoiceConnectionStatus.Ready, () => {
+			console.log('The connection has entered the Ready state - ready to play audio!');
+		});
+		const resource = createAudioResource(join(__dirname, 'test.mp3'), { inlineVolume: true });
+		player.play(resource);
+		connection.subscribe(player);
+
+		
+		player.on(AudioPlayerStatus.Playing, () => {
+			console.log('playing');
+		});
+		player.on(AudioPlayerStatus.Idle, () => {
+			console.log('Idle');
+		});
+
+}})
+
